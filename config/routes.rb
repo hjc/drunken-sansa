5272c505683583a9b248721011f96e4494b037c8
Hayden Chudy <hjc1710@gmail.com>
2013-01-17 00:13:19 -0600

    Added a Pages Controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 SampleApp::Application.routes.draw do
+  get "pages/home"
+
+  get "pages/contact"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -40,9 +44,9 @@ SampleApp::Application.routes.draw do
   #   end
 
   # Sample resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
+  #   namespace :contact do
+  #     # Directs /contact/products/* to Admin::ProductsController
+  #     # (app/controllers/contact/products_controller.rb)
   #     resources :products
   #   end
 
